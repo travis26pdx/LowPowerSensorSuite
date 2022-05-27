79d9dd5ad843dfe55fddfbc379adc27a8453d7dd
eeynis <59846716+eeynis@users.noreply.github.com>
2022-05-26 23:21:58 -0700

    REV 2
    Added N2O code into the main code (warm up time for N2O sensor is 1 minute). Also added new SD card code into the main code, where millis() function will get stored in the SD card file.

--- Firmware/Energia Code/SensorMote_V0.1/SensorMote_V0.1.ino
+++ Firmware/Energia Code/SensorMote_V0.1/SensorMote_V0.1.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <IpMtWrapper.h>
 #include "sdcard.h"
+#define N2OSerial Serial2
 #define HIH8120Addr         0x27                      //I2C Addr
 #define HIH8120Cmd          0xFF                      //HandShake Cmd
 #define BytesToRead         4                         //Lens of Read Bytes 
@@ -12,6 +13,7 @@
 unsigned long MeasureTimeTemp = 0;                    //Interval Between Last SampleNums And Now SampleNums
 unsigned long MeasureTimeHumi = 0;
 unsigned long MeasureTimeN2O = 0;
+unsigned long MeasureTimeN2OMinus1Minute = 0;
 unsigned long SampleTime = 0;                         //Sample Interval
 int SampleCounts = 0;                                 //Current Sample Counter
 /* Humiture */
@@ -23,29 +25,44 @@ float HumiAvg = 0;                                    //Calculate Average Accord
 float TempAvg = 0;
 float Humi,Temp;                                      //Humi (%) and Temp (C)
 bool dataSent = false;                                //Used to determine if MCU should go into sleep mode
+byte N2OCmd[7] = {0x10,0x13,0x06,0x10,0x1F,0x00,0x58};
+byte N2ORece[15];
+float N2OData;
+int minuteNine = 1;
 
 IpMtWrapper       ipmtwrapper;
 
 //============================== data generator ==================================
 void generateData(uint16_t* returnVal) {
+     returnVal[0] = 65535;                                       //Missing data filled with 65535 instead of 0 because 0 is valid data for N2O. Tried using -1 before but uint16_t data type only goes from 0 to 65535.
+     returnVal[1] = 65535;
+     returnVal[2] = 65535;
      if (millis() - MeasureTimeTemp >= MeasureIntervalTemp) {    //Measure temperature every 1 minute
          MeasureTimeTemp = millis();                             //Reset Timer
          GetHIHSensor();
-         returnVal[0] = (int)(Temp * 100);
-         returnVal[1] = 65535;                                   //Missing data filled with 65535 instead of 0 because 0 is valid data for N2O. Tried using -1 before but uint16_t data type only goes from 0 to 65535.
-         returnVal[2] = 65535;
-         SDwrite(SENSOR_TEMP, Temp);
+         returnVal[0] = (int)(Temp * 100);                       //Values multiplied by 100 because only integers can be sent via SmartMesh
+         SDwrite(SENSOR_TEMP, Temp, millis());
      }
      if (millis() - MeasureTimeHumi >= MeasureIntervalHumi) {    //Measure humidity every 2 minutes
          MeasureTimeHumi = millis();                             //Reset Timer
          returnVal[1] = (int)(Humi * 100);
-         returnVal[2] = 65535;
-         SDwrite(SENSOR_HUMI, Humi);
+         SDwrite(SENSOR_HUMI, Humi, millis());
+     }
+     if (millis() - MeasureTimeN2OMinus1Minute >= (MeasureIntervalN2O - 60000) && minuteNine) { //Sample rate of N2O minus 1 minute
+         MeasureTimeN2OMinus1Minute = millis();                  //Reset Timer
+         digitalWrite(P4_3, HIGH);                               //Apply 3.3V to gate of transistor
+         minuteNine = 0;
+     }
+     if (millis() - MeasureTimeN2OMinus1Minute >= MeasureIntervalN2O) {
+         MeasureTimeN2OMinus1Minute = millis();                  //Reset Timer
+         digitalWrite(P4_3, HIGH);                               //Apply 3.3V to gate of transistor
      }
      if (millis() - MeasureTimeN2O >= MeasureIntervalN2O) {      //Measure N2O every 10 minutes
          MeasureTimeN2O = millis();                              //Reset Timer
-         returnVal[2] = 1000;                                    //Placeholder value for N2O
-         SDwrite(SENSOR_NO2, returnVal[2]);
+         N2OGet();
+         returnVal[2] = (int)(N2OData * 100);
+         digitalWrite(P4_3, LOW);                                //Apply 0V to gate of transistor
+         SDwrite(SENSOR_NO2, N2OData, millis());
      }
      dataSent = true;
 }
@@ -61,17 +78,19 @@ void setup() {                                        //put your setup code here
   Serial.begin(9600);                                 //Init Serial
   Wire.begin();                                       //I2C Begin
   initSDCard();                                       //Init SD Card
+  N2OSerial.begin(38400);
+  pinMode(P4_3, OUTPUT);                              //Set P4_3 to be an output
+  digitalWrite(P4_3, LOW);                            //Must set P4_3 to low (0V) when defining pin mode for P4_3 because it will output logic level high (3.3V) by default.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (dataSent == true) {
-      sleep(60000);                                  //Have MCU sleep for 10 seconds after data has been sent
+      sleep(60000);                                  //Have MCU sleep for 60 seconds after data has been sent
   }
   dataSent = false;
   ipmtwrapper.loop();
 }
-
 
 void GetHIHSensor(){
   byte HIHdata[4];                                    //Store Data Read
@@ -107,6 +126,42 @@ void GetHIHSensor(){
   TempVals[SampleCounts] = Temp;
 }
 
+void N2OGet(){
+  N2OSerial.write(N2OCmd,7);
+  delay(1000);
+  if(N2OSerial.available()){
+    byte recebyte = N2OSerial.read();
+    Serial.print(recebyte, HEX);
+    Serial.print("|");
+    if(recebyte == 0x10){
+      N2ORece[0] = 0x10;
+      int i;
+      int m = 0;
+      char ReadArray[4];
+      bool flag;
+      while (N2OSerial.available()){
+        flag = false;
+        for(i = 1;i < 15;i++){
+          N2ORece[i] = N2OSerial.read();
+          Serial.print(N2ORece[i], HEX);
+          Serial.print("|");
+          if(i >= 7 && i <= 10){
+            ReadArray[m++] = N2ORece[i];
+          }
+        }
+        Serial.println();
+        N2OData = *(float*)ReadArray;
+        N2OData = N2OData / 10;
+        Serial.print("N2O Data is: ");
+        Serial.print(N2OData);
+        Serial.println("ppm");
+        if(N2OData == -2500){
+          Serial.println("N2O Sensor Warmup!");
+        }
+      }
+    }
+  } 
+}
 
 void CalculateAvg(){
   /* Humiture */
